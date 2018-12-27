e4a9c34f6b14bd204ac4fc9025bd90d03938cef9
archer-bao <20513593+archer-bao@users.noreply.github.com>
2018-12-27 15:00:18 +0800

    Update fileTransfer.go

--- Golang/fileTransfer.go
+++ Golang/fileTransfer.go
@@ -19,7 +19,6 @@ var (
 		</br></br>
 		<form enctype="multipart/form-data" action="/upload" method="post">
 		<input type="file" name="uploadfile" />
-		<input type="hidden" name="token" value="{{.}}"/>
 		<input type="submit" value="upload" />
 		</form>
 	</body>
